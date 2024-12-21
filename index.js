cb26225992c012e19c900547bcfc7c373c3d2dfb
Adrian Jade Manaday <adrianjademanaday@gmail.com>
2024-12-21 10:30:17 +0800

    Refactored the /bearerToken API endpoint

--- index.js
+++ index.js
@@ -57,14 +57,16 @@ app.get("/apiKey", async (req, res) => {
   }
 });
 
+const configToken = {
+  headers: {
+    Authorization: `Bearer ${yourBearerToken}`,
+  }
+}
+
 app.get("/bearerToken", async (req, res) => {
 
   try {
-    const result = await axios.get(`${API_URL}/secrets/42`, {
-      headers: {
-        Authorization: `Bearer ${yourBearerToken}`
-      }
-    });
+    const result = await axios.get(`${API_URL}/secrets/42`, configToken);
     res.render("index.ejs", { content: JSON.stringify(result.data) });
   } catch (error) {
     res.status(404).send(error.message);
